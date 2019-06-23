3ce0c002283f217054d75cdb4bae09e463141263
Boy Maas <boy.maas@gmail.com>
2019-06-23 18:20:35 +0200

    passThroughCopy

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,8 @@ module.exports = function(eleventyConfig) {
   // eleventyConfig.addPassthroughCopy("img");
   // eleventyConfig.addPassthroughCopy("css");
 
+  eleventyConfig.addPassthroughCopy("src/CNAME");
+
   return {
       dir: { input: 'src', output: '_site', data: '_data' },
       passthroughFileCopy: true
