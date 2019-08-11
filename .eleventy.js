14ede3906f310167a6b26690a582ea34059743e7
Boy Maas <boy.maas@gmail.com>
2019-08-11 14:09:32 +0200

    update, qr-code and other stuff

--- .eleventy.js
+++ .eleventy.js
@@ -9,7 +9,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img");
   // eleventyConfig.addPassthroughCopy("css");
 
+
+  eleventyConfig.addPassthroughCopy("src/cninfo.json");
   eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.addPassthroughCopy("src/js");
 
   return {
       dir: { input: 'src', output: '_site', data: '_data' },
